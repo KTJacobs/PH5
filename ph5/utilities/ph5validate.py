fc906d55ce9e645f2ade335eaca54f9bc361438c
Nicholas Falco <nick@iris.washington.edu>
2018-04-06 15:09:51 -0700

    - Add ability to specify output file in ph5_validate. - Add check to see if multiple experiment records exists in experiment_t - Add warning logging level - Move check for channels without data to the warning logging level. - Improve error handling to avoid having users see a stack trace.

--- ph5/utilities/ph5validate.py
+++ ph5/utilities/ph5validate.py
@@ -9,21 +9,33 @@ import subprocess
 import sys
 from ph5.core import ph5api
 
-PROG_VERSION = "2017.344"
+PROG_VERSION = "2018.096"
+
+
+class PH5ValidateException(Exception):
+    pass
 
 
 class PH5Validate(object):
-    def __init__(self, ph5API_object, ph5path, level):
-        if level == "error":
-            logging.basicConfig(filename='PH5Validate.log',
+    def __init__(self, ph5API_object, ph5path,
+                 level, outfile):
+        if level == "ERROR":
+            logging.basicConfig(filename=outfile,
                                 format='%(levelname)s: %(message)s',
                                 filemode='w',
                                 level=logging.ERROR)
-        else:
-            logging.basicConfig(filename='PH5Validate.log',
+        elif level == "WARNING":
+            logging.basicConfig(filename=outfile,
+                                format='%(levelname)s: %(message)s',
+                                filemode='w',
+                                level=logging.WARNING)
+        elif level == "DEBUG":
+            logging.basicConfig(filename=outfile,
                                 format='%(levelname)s: %(message)s',
                                 filemode='w',
                                 level=logging.DEBUG)
+        else:
+            raise PH5ValidateException("Invalid Level %s" % level)
 
         self.ph5 = ph5API_object
         self.path = ph5path
@@ -55,40 +67,44 @@ class PH5Validate(object):
         experiment_t = self.ph5.Experiment_t['rows']
         logging.info("Checking Experiment Table")
         if len(experiment_t) == 0:
-            logging.error("Experiment_t does not exist. " +
+            logging.error("Experiment_t does not exist. "
                           "run experiment_t_gen to create table")
             return
+        if len(experiment_t) > 1:
+            logging.error("Metadata for more than one experiment found "
+                          "in experiment_t")
+            return
         if not experiment_t[0]['net_code_s']:
-            logging.error('Network code was not found: ' +
+            logging.error('Network code was not found: '
                           'A 2 character network code is required.')
 
         if not experiment_t[0]['experiment_id_s']:
-            logging.error('Experiment ID was not found: ' +
+            logging.error('Experiment ID was not found: '
                           'An experiment id (YY-XXX) is required.')
 
         if not experiment_t[0]['nickname_s']:
-            logging.warning('Nickname was not found: ' +
-                            'It is suggested you include a nickname ' +
+            logging.warning('Nickname was not found: '
+                            'It is suggested you include a nickname '
                             'for your experiment')
 
         if not experiment_t[0]['longname_s']:
-            logging.warning('long name was not found: ' +
-                            'It is suggested you include a long name ' +
+            logging.warning('long name was not found: '
+                            'It is suggested you include a long name '
                             'for your experiment')
 
         if not experiment_t[0]['PIs_s']:
-            logging.warning('PIs were not found: ' +
-                            'It is suggested you include the PIs ' +
+            logging.warning('PIs were not found: '
+                            'It is suggested you include the PIs '
                             'for your experiment')
 
         if not experiment_t[0]['institutions_s']:
-            logging.warning("Institutions were not found: " +
-                            "It is suggested you include the institutions " +
+            logging.warning("Institutions were not found: "
+                            "It is suggested you include the institutions "
                             "for your experiment")
 
         if (experiment_t[0]['north_west_corner/X/value_d'] == 0.0):
-            logging.warning('A bounding box was not detected: ' +
-                            'A suggested bounding box has been calculated ' +
+            logging.warning('A bounding box was not detected: '
+                            'A suggested bounding box has been calculated '
                             'and saved in experiment_t_bounding_box.kef')
             max_lat = None
             min_lat = None
@@ -158,10 +174,8 @@ class PH5Validate(object):
                         min_lon = longitude
 
             process = subprocess.Popen(
-                "ph5tokef -n master.ph5 -p " +
-                str(self.path) +
-                " -E >" +
-                "experiment_t_bounding_box.kef",
+                "ph5tokef -n master.ph5 -p %s -E >"
+                "experiment_t_bounding_box.kef" % (str(self.path)),
                 shell=True,
                 stdout=subprocess.PIPE)
             process.wait()
@@ -170,17 +184,17 @@ class PH5Validate(object):
                 kef = f.readlines()
             for line in kef:
                 if line.startswith("	north_west_corner/X/value_d="):
-                    new_kef.append("	north_west_corner/X/value_d=" +
-                                   str(min_lon) + "\n")
+                    new_kef.append("	north_west_corner/X/value_d=%s\n" %
+                                   str(min_lon))
                 elif line.startswith("	north_west_corner/Y/value_d="):
-                    new_kef.append("	north_west_corner/Y/value_d=" +
-                                   str(max_lat) + "\n")
+                    new_kef.append("	north_west_corner/Y/value_d=%s\n" %
+                                   str(max_lat))
                 elif line.startswith("	south_east_corner/X/value_d="):
-                    new_kef.append("	south_east_corner/X/value_d=" +
-                                   str(max_lon) + "\n")
+                    new_kef.append("	south_east_corner/X/value_d=%s\n" %
+                                   str(max_lon))
                 elif line.startswith("	south_east_corner/Y/value_d"):
-                    new_kef.append("	south_east_corner/Y/value_d=" +
-                                   str(min_lat) + "\n")
+                    new_kef.append("	south_east_corner/Y/value_d=%s\n" %
+                                   str(min_lat))
                 else:
                     new_kef.append(line)
             outfile = open("experiment_t_bounding_box.kef", 'w')
@@ -188,7 +202,7 @@ class PH5Validate(object):
                 outfile.write("%s" % line)
         return
 
-    def checK_stations(self):
+    def check_stations(self):
             logging.info("Checking Stations...")
             array_names = sorted(self.ph5.Array_t_names)
 
@@ -207,9 +221,9 @@ class PH5Validate(object):
                                 st_num]['das/serial_number_s']
                             channel = station_list[deployment][
                                 st_num]['channel_number_i']
-                            logging.info("\n##############")
-                            logging.info("Station " + str(station_id) +
-                                         " Channel " + str(channel) + "\n")
+                            logging.info("Station %s Channel %s"
+                                         % (str(station_id),
+                                            str(channel)))
                             if not station_list[deployment][
                                    st_num]['seed_station_name_s']:
                                 logging.error("SEED station name required.")
@@ -218,7 +232,7 @@ class PH5Validate(object):
                                          station_list[deployment][
                                              st_num]['response_table_n_i'])
                             if response_t is None:
-                                logging.error("No Response table found." +
+                                logging.error("No Response table found."
                                               "Have you run load_resp yet?")
 
                             deploy_time = station_list[deployment][
@@ -230,28 +244,28 @@ class PH5Validate(object):
                                                 pickup_time, reread=False)
 
                             if serial not in self.ph5.Das_t:
-                                logging.error("No Data found for: " +
-                                              str(station_id) +
-                                              " You may need " +
-                                              "to reload the raw data for " +
-                                              "this station.")
+                                logging.error("No data found for: %s"
+                                              " You may need "
+                                              "to reload the raw data for "
+                                              "this station."
+                                              % (str(station_id)))
                             try:
                                 ph5api.filter_das_t(self.ph5.Das_t[
                                                     serial]['rows'],
                                                     channel)
                             except:
-                                logging.error("NO Data found for channel " +
-                                              str(channel) +
-                                              " Other channels seem to exist")
+                                logging.warning("No data found for channel %s"
+                                                " Other channels seem to exist"
+                                                % (str(channel)))
 
                             if station_list[deployment][
                                          st_num]['sample_rate_i'] == 0:
-                                logging.warning("Sample rate seems to be 0." +
+                                logging.warning("Sample rate seems to be 0."
                                                 " Is this correct???")
                             if station_list[deployment][
                                          st_num][
                                          'sample_rate_multiplier_i'] == 0:
-                                logging.warning("Sample rate multiplier 0." +
+                                logging.warning("Sample rate multiplier 0."
                                                 " Is this correct???")
 
             return
@@ -263,11 +277,11 @@ def get_args():
 
     parser = argparse.ArgumentParser(
         description='Runs set of checks on PH5 archvive',
-        usage='Version: {0} ph5validate--nickname="Master_PH5_file" [options]'
-        .format(PROG_VERSION))
+        usage=('Version: %s ph5validate--nickname="Master_PH5_file" [options]'
+               % (PROG_VERSION)))
 
     parser.add_argument(
-        "-n", "--nickname", action="store", required=True,
+        "-n", "--nickname", action="store", default="master.ph5",
         type=str, metavar="nickname")
 
     parser.add_argument(
@@ -275,22 +289,36 @@ def get_args():
         type=str, metavar="ph5_path")
 
     parser.add_argument(
-        "-l", "--level", action="store", default="error",
-        type=str, metavar="level")
+        "-l", "--level", action="store", default="ERROR",
+        type=str, metavar="level",
+        help="Level of logging detail. Choose from ERROR, WARNING, DEBUG")
+
+    parser.add_argument("-o", "--outfile", action="store",
+                        default="PH5Validate.log", type=str,
+                        metavar="outfile",
+                        )
 
     the_args = parser.parse_args()
     return the_args
 
 
 def main():
-    args = get_args()
-    ph5API_object = ph5api.PH5(path=args.ph5path, nickname=args.nickname,)
-    ph5validate = PH5Validate(ph5API_object, args.ph5path, level=args.level)
-    ph5validate.check_experiment_t()
-    ph5validate.checK_stations()
-    ph5API_object.close()
-    sys.stdout.write("\nWarnings, Errors and suggests written to logfile: " +
-                     "PH5Validate.log\n\n")
+    try:
+        args = get_args()
+        ph5API_object = ph5api.PH5(path=args.ph5path, nickname=args.nickname,)
+        ph5validate = PH5Validate(ph5API_object,
+                                  args.ph5path,
+                                  args.level.upper(),
+                                  args.outfile)
+        ph5validate.check_experiment_t()
+        ph5validate.check_stations()
+        ph5API_object.close()
+        sys.stdout.write("\nWarnings, Errors and suggestions written "
+                         "to logfile: %s\n\n" % (args.outfile))
+    except ph5api.APIError as err:
+        sys.stdout.write("%s\n" % err)
+    except PH5ValidateException as err:
+        sys.stdout.write("%s\n" % err)
 
 
 if __name__ == '__main__':
